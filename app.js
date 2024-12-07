51d4306e5b3f452438f4c0f24d5f2a568102d669
Nikhil Pathak <nikhilpathak2602@gmail.com>
2024-12-07 20:46:51 +0530

    Creating Ejs View

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ const app=new express();
 const mongoose=require("mongoose");
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")));
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
 
 app.listen(3000,()=>{
     console.log("Server Running at port 3000");
