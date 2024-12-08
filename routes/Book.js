f575f359a4d3101adab961b280b2bb1706ed1a52
Nikhil Pathak <nikhilpathak2602@gmail.com>
2024-12-08 21:21:13 +0530

    Redis Chached Data

--- routes/Book.js
+++ routes/Book.js
@@ -1,37 +1,45 @@
-const express=require("express");
-const bodyparser=require("body-parser");
-const Books=require("../model/BookSchema");
-const BookRouter=express.Router();
+const express = require("express");
+const bodyparser = require("body-parser");
+const Books = require("../model/BookSchema");
+const BookRouter = express.Router();
+const client = require("../redis");
 
 BookRouter.use(bodyparser.json());
 
-BookRouter.get("/",async (req,res)=>{
-    try
-    {
-        const books=await Books.find({});
-        res.render("book",{
-            books:books
-        });
+BookRouter.get("/", async (req, res) => {
+  try {
+    const result = await client.get("books");
+    if (result) {
+      const books=JSON.parse(result);
+      console.log("Redis stored data");
+      res.render("book", {
+        books: books
+      });
+    } else {
+      const books = await Books.find({});
+      await client.set("books",JSON.stringify(books));
+      await client.expire("books",100);
+      console.log("from server");
+      res.render("book", {
+        books: books
+      });
     }
-    catch(err)
-    {
-        res.status(500).json({err:err.message});
-    }
+  } catch (err) {
+    res.status(500).json({ err: err });
+  }
 });
 
-BookRouter.post("/",async (req,res)=>{
-    try{
-        const result=await Books.create({
-            name:req.body.name,
-            author:req.body.author,
-            genre:req.body.genre
-        });
-        res.status(200).json({result:result});
-    }
-    catch(err)
-    {
-        res.status(500).json({err:err.message});
-    }
-})
+BookRouter.post("/", async (req, res) => {
+  try {
+    const result = await Books.create({
+      name: req.body.name,
+      author: req.body.author,
+      genre: req.body.genre,
+    });
+    res.status(200).json({ result: result });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
 
-module.exports=BookRouter;
+module.exports = BookRouter;
--- routes/Cart.js
+++ routes/Cart.js
@@ -1,13 +1,44 @@
-const express=require("express");
-const bodyparser=require("body-parser");
-const Cart=require("../model/CartSchema");
-const CartRouter=express.Router();
+const express = require("express");
+const bodyparser = require("body-parser");
+const Cart = require("../model/CartSchema");
+const CartRouter = express.Router();
+const client=require('../redis');
 
 CartRouter.use(bodyparser.json());
 
-
-CartRouter.get("/",(req,res)=>{
-    res.render("cart");
+CartRouter.get("/", async (req, res) => {
+  try {
+    const result = await client.get("carts");
+    if (result) {
+      const carts = JSON.parse(result);
+      console.log("Redis stored data");
+      res.render("cart", {
+        carts: carts,
+      });
+    } else {
+      const carts = await Cart.find({});
+      await client.set("carts", JSON.stringify(carts));
+      await client.expire("carts", 60);
+      console.log("from server");
+      res.render("cart", {
+        carts: carts,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
 });
 
-module.exports=CartRouter;
+CartRouter.post("/", async (req, res) => {
+  try {
+    const result = await Cart.create({
+      name: req.body.name,
+      quantity: req.body.quantity,
+    });
+    res.status(200).json({ result: result });
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+module.exports = CartRouter;
